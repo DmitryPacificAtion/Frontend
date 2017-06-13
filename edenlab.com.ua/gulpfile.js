67ac54db3445b704dbb8b4cc9c2badbdda181a29
Dmitry Bruslik <dmitry.bruslik@yandex.ua>
2017-06-13 11:01:51 +0300

    Пытаюсь собрать проект

--- edenlab.com.ua/gulpfile.js
+++ edenlab.com.ua/gulpfile.js
@@ -1,13 +1,22 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var jq = require('jquery');
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	jq = require('jquery'),
+	watch = require('gulp-watch');
+
+var paths = {
+  sass: ['app/scss/**/*.scss']
+};
 
 gulp.task('sass', function(){
-	return gulp.src('./app/sass/*.scss')
+	return gulp.src(paths)
 	.pipe(scss())
-	.pipe(gulp.dest('./build/css'));
+	.pipe(gulp.dest('build/css'));
 	});
 
-gulp.task('watch', 'sass');
+gulp.task('watch', function() {
+  gulp.watch(paths.sass, ['sass']);
+});
+
+gulp.task('default', ['watch']);
 
 //TODO: а работает ли?
